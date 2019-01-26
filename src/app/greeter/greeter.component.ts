2d43578c6788475a2dffba55948f72e38777db2b
Audre <audre.barellano@example.com>
2019-01-26 11:36:00 +0800

    first commit

---
+++ src/app/greeter/greeter.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greeter',
+  templateUrl: './greeter.component.html',
+  styleUrls: ['./greeter.component.css']
+})
+export class GreeterComponent implements OnInit {
+  name = 'World';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+   
+}
